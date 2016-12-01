58d82b62efc9eca76504f70646c83edc518d53c8
ridwanskaterocks <ridwanskaterocks@gmail.com>
2016-12-01 14:30:18 +0700

    Setter getter path strorage

--- js/fireNotif.js
+++ js/fireNotif.js
@@ -5,3 +5,38 @@
 * @since 		2016
 * @license 		MIT
 */
+
+/**
+* Class FireNotif
+*
+* @param String	url
+* @param String	path
+*/
+function FireNotif(url, path) {
+	if (typeof url != undefined) {
+		this.firebaseUrl = url;
+	}
+
+	if (typeof path != undefined) {
+		this.setPath(path);
+	}
+}
+
+/**
+* Set path storage
+*
+* @param String	path
+*/
+Firenotif.prototype.setPath = function(path) {
+	this.pathName = path;
+
+	return this;
+}
+
+/**
+* Get path storage
+*
+*/
+Firenotif.prototype.getPath = function() {
+	return this.pathName;
+}
